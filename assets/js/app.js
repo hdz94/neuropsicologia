0640ac7f416c35624b6d804bd9117594993780e9
Jesus hdz <hdz.class94@gmail.com>
2020-04-23 01:44:30 -0500

    deleted neuropsicologia

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 
 /* Otherwise just put the config content (json): */
-particlesJS.load('particles-js', '/../neuropsicologia/assets/particles.json', function() {
+particlesJS.load('particles-js', '/../assets/particles.json', function() {
   console.log('callback - particles.json config loaded');
 });
