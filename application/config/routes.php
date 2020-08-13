0315264c660a180b7a07764c261abb5bdffe8cc9
Jesus hdz <hdz.class94@gmail.com>
2020-08-13 01:16:12 -0500

    require memcached ext

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'inicio';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['sessionex'] = 'Session_Controller';
+//$route['sessionex'] = 'Session_Controller';
