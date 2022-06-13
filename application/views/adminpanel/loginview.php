eda064b2f9de85e2f3ed1b047d22ebc77708b53a
hdz94 <hdz.class94@gmail.com>
2022-06-13 14:36:26 -0500

    date update

--- application/views/adminpanel/loginview.php
+++ application/views/adminpanel/loginview.php
@@ -37,7 +37,7 @@
   	    </div>
   	  
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  	    <p class="mt-5 mb-3 text-muted">© 2017-2020</p>
+  	    <p class="mt-5 mb-3 text-muted">© 2021-2022</p>
 	    </form>
 
     <!-- Optional JavaScript -->
